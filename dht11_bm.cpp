f25755724235a349b7aa5134daa700656d2253e3
Luciano Marelli <lucianomarelli94@gmail.com>
2021-07-30 18:12:41 -0300

    Implemeto Mock DHT11

---
+++ dht11_bm.cpp
@@ -0,0 +1,12 @@
+#include "dht11_bm.h"
+
+float dht11_get(){
+    float value;
+    value = 25.0;
+
+    return value;
+}
+
+void dht11_config(int value){
+    return;
+}
---
+++ dht11_bm.h
@@ -0,0 +1,8 @@
+#ifndef __DHT11_BM_H__
+#define __DHT11_BM_H__
+
+// Funciones publicas
+float dht11_get();
+void dht11_config(int value);
+
+#endif
