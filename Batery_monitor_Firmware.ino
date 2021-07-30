cece607f1ec7023af9b38ba4d08256ea190df5a9
Luciano Marelli <lucianomarelli94@gmail.com>
2021-07-30 18:05:46 -0300

    Enciendo y apago el relay

--- Batery_monitor_Firmware.ino
+++ Batery_monitor_Firmware.ino
@@ -1,12 +1,15 @@
+#define RELAY_1 5
 
 void setup()
 {
     Serial.begin(115200);
     Serial.println("Inicia programa");
+    pinMode(RELAY_1, OUTPUT);
 }
 
 void loop()
 {
     Serial.println("[LOOP]: mensaje de testeo.");
+    digitalWrite(RELAY_1, !digitalRead(RELAY_1));
     delay(2000);
 }
